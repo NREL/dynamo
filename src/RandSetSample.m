a8a3af58ccc44b3d6c8f0c934e328ec56c7514c3
Bryan Palmintier <bryan.palmintier@nrel.gov>
2017-04-06 00:20:50 -0600

    Swap order of call to sample in RandSetSample

--- src/RandSetSample.m
+++ src/RandSetSample.m
@@ -8,6 +8,7 @@ function u_list = RandSetSample(cell_of_RandProcess, t, n_samples, opt)
 % HISTORY
 % ver     date    time       who     changes made
 % ---  ---------- -----  ----------- ---------------------------------------
+%   2  2017-04-06 00:05  BryanP      Adjusted order of calls to sample
 %   1  2016-10-27 14:05  BryanP      Initial Code
 
 if nargin < 3 || isempty(n_samples)
@@ -22,5 +23,5 @@ end
 % Note: not preallocating b/c not sure of type
 u_list = [];
 for idx = 1:length(cell_of_RandProcess)
-    u_list = [u_list, cell_of_RandProcess{idx}.sample(t, n_samples, opt)]; %#ok<AGROW>
+    u_list = [u_list, cell_of_RandProcess{idx}.sample(n_samples, t, opt)]; %#ok<AGROW>
 end
