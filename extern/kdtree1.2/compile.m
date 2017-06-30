b8d0772ca9294b407d80d40face194b387b1fadb
NREL_NT\jbukenbe <Jesse.Bukenberger@nrel.gov>
2017-06-30 09:04:23 -0600

    Consolidated kdtree into only having most recent (2012) version

--- extern/kdtree1.2/compile.m
+++
@@ -1,8 +0,0 @@
-% Simple script to compile all required functions
-
-mex kdtree_ball_query.cpp
-mex kdtree_build.cpp
-mex kdtree_delete.cpp
-mex kdtree_k_nearest_neighbors.cpp
-mex kdtree_nearest_neighbor.cpp
-mex kdtree_range_query.cpp
